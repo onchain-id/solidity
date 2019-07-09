18b0b351b8c50fa5b08b8652fa12667568e9db0f
THIZY Kevin <kevin.thizy@outlook.fr>
2019-07-09 09:41:34 +0200

    🔧 Configure truffle and add scripts

---
+++ truffle-config.js
@@ -0,0 +1,24 @@
+const solcStable = {
+  version: '0.5.7',
+};
+
+module.exports = {
+  networks: {
+    development: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*', // eslint-disable-line camelcase
+    },
+    coverage: {
+      host: 'localhost',
+      network_id: '*', // eslint-disable-line camelcase
+      port: 8555,
+      gas: 0xfffffffffff,
+      gasPrice: 0x01,
+    },
+  },
+
+  compilers: {
+    solc: solcStable,
+  },
+};
