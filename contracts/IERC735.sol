6c39ca23f57fd2a9ab8c25c4206022eeee231de8
THIZY Kevin <kevin.thizy@outlook.fr>
2019-07-09 18:12:34 +0200

    🐛 Rename correctly claimType to topic

--- contracts/IERC735.sol
+++ contracts/IERC735.sol
@@ -10,28 +10,28 @@ interface IERC735 {
      *
      * Specification: Is not clear
      */
-    event ClaimRequested(uint256 indexed claimRequestId, uint256 indexed claimType, uint256 scheme, address indexed issuer, bytes signature, bytes data, string uri);
+    event ClaimRequested(uint256 indexed claimRequestId, uint256 indexed topic, uint256 scheme, address indexed issuer, bytes signature, bytes data, string uri);
 
     /**
      * @dev Emitted when a claim was added.
      *
      * Specification: MUST be triggered when a claim was successfully added.
      */
-    event ClaimAdded(bytes32 indexed claimId, uint256 indexed claimType, uint256 scheme, address indexed issuer, bytes signature, bytes data, string uri);
+    event ClaimAdded(bytes32 indexed claimId, uint256 indexed topic, uint256 scheme, address indexed issuer, bytes signature, bytes data, string uri);
 
     /**
      * @dev Emitted when a claim was removed.
      *
      * Specification: MUST be triggered when removeClaim was successfully called.
      */
-    event ClaimRemoved(bytes32 indexed claimId, uint256 indexed claimType, uint256 scheme, address indexed issuer, bytes signature, bytes data, string uri);
+    event ClaimRemoved(bytes32 indexed claimId, uint256 indexed topic, uint256 scheme, address indexed issuer, bytes signature, bytes data, string uri);
 
     /**
      * @dev Emitted when a claim was changed.
      *
      * Specification: MUST be triggered when changeClaim was successfully called.
      */
-    event ClaimChanged(bytes32 indexed claimId, uint256 indexed claimType, uint256 scheme, address indexed issuer, bytes signature, bytes data, string uri);
+    event ClaimChanged(bytes32 indexed claimId, uint256 indexed topic, uint256 scheme, address indexed issuer, bytes signature, bytes data, string uri);
 
     /**
      * @dev Definition of the structure of a Claim.
@@ -47,7 +47,7 @@ interface IERC735 {
      *      - uri: The location of the claim, this can be HTTP links, swarm hashes, IPFS hashes, and such.
      */
     struct Claim {
-        uint256 claimType;
+        uint256 topic;
         uint256 scheme;
         address issuer;
         bytes signature;
@@ -60,12 +60,12 @@ interface IERC735 {
      *
      * Claim IDs are generated using `keccak256(abi.encodePacked(address issuer_address + uint256 topic))`.
      */
-    function getClaim(bytes32 _claimId) external view returns(uint256 claimType, uint256 scheme, address issuer, bytes memory signature, bytes memory data, string memory uri);
+    function getClaim(bytes32 _claimId) external view returns(uint256 topic, uint256 scheme, address issuer, bytes memory signature, bytes memory data, string memory uri);
 
     /**
      * @dev Returns an array of claim IDs by topic.
      */
-    function getClaimIdsByTopic(uint256 _claimType) external view returns(bytes32[] memory claimIds);
+    function getClaimIdsByTopic(uint256 _topic) external view returns(bytes32[] memory claimIds);
 
     /**
      * @dev Add or update a claim.
@@ -81,7 +81,7 @@ interface IERC735 {
      * This COULD implement an approval process for pending claims, or add them right away.
      * MUST return a claimRequestId (use claim ID) that COULD be sent to the approve function.
      */
-    function addClaim(uint256 _claimType, uint256 _scheme, address issuer, bytes calldata _signature, bytes calldata _data, string calldata _uri) external returns (bytes32 claimRequestId);
+    function addClaim(uint256 _topic, uint256 _scheme, address issuer, bytes calldata _signature, bytes calldata _data, string calldata _uri) external returns (bytes32 claimRequestId);
 
     /**
      * @dev Removes a claim.
--- contracts/Identity.sol
+++ contracts/Identity.sol
@@ -9,18 +9,18 @@ import "./IERC735.sol";
 contract Identity is ERC734, IERC735 {
 
     mapping (bytes32 => Claim) private claims;
-    mapping (uint256 => bytes32[]) private claimsByType;
+    mapping (uint256 => bytes32[]) private claimsByTopic;
 
     /**
        * @notice Implementation of the addClaim function from the ERC-735 standard
        *  Require that the msg.sender has claim signer key.
        *
-       * @param _claimType The type of claim
+       * @param _topic The type of claim
        * @param _scheme The scheme with which this claim SHOULD be verified or how it should be processed.
        * @param _issuer The issuers identity contract address, or the address used to sign the above signature.
-       * @param _signature Signature which is the proof that the claim issuer issued a claim of claimType for this identity.
-       * it MUST be a signed message of the following structure: keccak256(address identityHolder_address, uint256 _ claimType, bytes data)
-       * or keccak256(abi.encode(identityHolder_address, claimType, data))
+       * @param _signature Signature which is the proof that the claim issuer issued a claim of topic for this identity.
+       * it MUST be a signed message of the following structure: keccak256(address identityHolder_address, uint256 _ topic, bytes data)
+       * or keccak256(abi.encode(identityHolder_address, topic, data))
        * @param _data The hash of the claim data, sitting in another location, a bit-mask, call data, or actual data based on the claim scheme.
        * @param _uri The location of the claim, this can be HTTP links, swarm hashes, IPFS hashes, and such.
        *
@@ -29,7 +29,7 @@ contract Identity is ERC734, IERC735 {
        */
 
     function addClaim(
-        uint256 _claimType,
+        uint256 _topic,
         uint256 _scheme,
         address _issuer,
         bytes memory _signature,
@@ -39,17 +39,17 @@ contract Identity is ERC734, IERC735 {
     public
     returns (bytes32 claimRequestId)
     {
-        bytes32 claimId = keccak256(abi.encodePacked(_issuer, _claimType));
+        bytes32 claimId = keccak256(abi.encodePacked(_issuer, _topic));
 
         if (msg.sender != address(this)) {
             require(keyHasPurpose(keccak256(abi.encodePacked(msg.sender)), 3), "Sender does not have claim signer key");
         }
 
         if (claims[claimId].issuer != _issuer) {
-            claimsByType[_claimType].push(claimId);
+            claimsByTopic[_topic].push(claimId);
         }
 
-        claims[claimId].claimType = _claimType;
+        claims[claimId].topic = _topic;
         claims[claimId].scheme = _scheme;
         claims[claimId].issuer = _issuer;
         claims[claimId].signature = _signature;
@@ -58,7 +58,7 @@ contract Identity is ERC734, IERC735 {
 
         emit ClaimAdded(
             claimId,
-            _claimType,
+            _topic,
             _scheme,
             _issuer,
             _signature,
@@ -74,7 +74,7 @@ contract Identity is ERC734, IERC735 {
        * Require that the msg.sender has management key.
        * Can only be removed by the claim issuer, or the claim holder itself.
        *
-       * @param _claimId The identity of the claim i.e. keccak256(address issuer_address + uint256 claimType)
+       * @param _claimId The identity of the claim i.e. keccak256(address issuer_address + uint256 topic)
        *
        * @return Returns TRUE when the claim was removed.
        * triggers ClaimRemoved event
@@ -87,7 +87,7 @@ contract Identity is ERC734, IERC735 {
 
         emit ClaimRemoved(
             _claimId,
-            claims[_claimId].claimType,
+            claims[_claimId].topic,
             claims[_claimId].scheme,
             claims[_claimId].issuer,
             claims[_claimId].signature,
@@ -95,7 +95,7 @@ contract Identity is ERC734, IERC735 {
             claims[_claimId].uri
         );
 
-        bytes32[] memory claimList = claimsByType[claims[_claimId].claimType];
+        bytes32[] memory claimList = claimsByTopic[claims[_claimId].topic];
 
         uint claimIndex = 0;
 
@@ -116,16 +116,16 @@ contract Identity is ERC734, IERC735 {
     /**
         * @notice Implementation of the getClaim function from the ERC-735 standard.
         *
-        * @param _claimId The identity of the claim i.e. keccak256(address issuer_address + uint256 claimType)
+        * @param _claimId The identity of the claim i.e. keccak256(address issuer_address + uint256 topic)
         *
-        * @return Returns all the parameters of the claim for the specified _claimId (claimType, scheme, signature, issuer, data, uri) .
+        * @return Returns all the parameters of the claim for the specified _claimId (topic, scheme, signature, issuer, data, uri) .
         */
 
     function getClaim(bytes32 _claimId)
     public
     view
     returns(
-        uint256 claimType,
+        uint256 topic,
         uint256 scheme,
         address issuer,
         bytes memory signature,
@@ -134,7 +134,7 @@ contract Identity is ERC734, IERC735 {
     )
     {
         return (
-        claims[_claimId].claimType,
+        claims[_claimId].topic,
         claims[_claimId].scheme,
         claims[_claimId].issuer,
         claims[_claimId].signature,
@@ -145,18 +145,18 @@ contract Identity is ERC734, IERC735 {
 
     /**
         * @notice Implementation of the getClaimIdsByTopic function from the ERC-735 standard.
-        * used to get all the claims from the specified claimType
+        * used to get all the claims from the specified topic
         *
-        * @param _claimType The identity of the claim i.e. keccak256(address issuer_address + uint256 claimType)
+        * @param _topic The identity of the claim i.e. keccak256(address issuer_address + uint256 topic)
         *
-        * @return Returns an array of claim IDs by claimType.
+        * @return Returns an array of claim IDs by topic.
         */
 
-    function getClaimIdsByType(uint256 _claimType)
+    function getClaimIdsByTopic(uint256 _topic)
     public
     view
     returns(bytes32[] memory claimIds)
     {
-        return claimsByType[_claimType];
+        return claimsByTopic[_topic];
     }
 }
