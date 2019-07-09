d8bf900598404d9a950acbd45df3097e8219e27f
THIZY Kevin <kevin.thizy@outlook.fr>
2019-07-09 14:46:12 +0200

    ✨ Export contracts

---
+++ index.js
@@ -0,0 +1,13 @@
+const IERC734 = require('./build/contracts/IERC734');
+const IERC735 = require('./build/contracts/IERC734');
+const ERC734 = require('./build/contracts/ERC734');
+const Identity = require('./build/contracts/Identity');
+
+module.exports = {
+  contracts: {
+    ERC734,
+    Identity,
+    IERC734,
+    IERC735,
+  },
+};
