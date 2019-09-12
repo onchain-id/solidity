d211eae8bbdc8bebb3a3790ba9ced880758a1024
THIZY Kevin <kevin.thizy@outlook.fr>
2019-09-12 15:43:00 +0200

    🐛(0.2.3) Wrong export for IERC735

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const IERC734 = require('./build/contracts/IERC734');
-const IERC735 = require('./build/contracts/IERC734');
+const IERC735 = require('./build/contracts/IERC735');
 const ERC734 = require('./build/contracts/ERC734');
 const Identity = require('./build/contracts/Identity');
 
