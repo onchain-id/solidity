9451a39b9f028391c1943aeedae5e2aa058ef726
THIZY Kevin <kevin.thizy@outlook.fr>
2019-09-17 09:43:19 +0200

    (0.3.0) Add Claim Issuer contract and interface

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
+const IClaimIssuer = require('./build/contracts/IClaimIssuer');
 const IERC734 = require('./build/contracts/IERC734');
 const IERC735 = require('./build/contracts/IERC735');
+const ClaimIssuer = require('./build/contracts/ClaimIssuer');
 const ERC734 = require('./build/contracts/ERC734');
 const Identity = require('./build/contracts/Identity');
 
 module.exports = {
   contracts: {
+    ClaimIssuer,
     ERC734,
     Identity,
   },
   interfaces: {
+    IClaimIssuer,
     IERC734,
     IERC735,
   },
