6625c329bd84fe3ab5b9c85565c9f0059b1905ff
Adam <boudjemaa.adam@gmail.com>
2020-11-13 19:48:22 +0100

    :add:(Truffle-Proxy) Add truffle-proxy

--- contracts/IERC735.sol
+++ contracts/IERC735.sol
@@ -1,4 +1,5 @@
-pragma solidity ^0.6.2;
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.6.9;
 
 /**
  * @dev Interface of the ERC735 (Claim Holder) standard as defined in the EIP.
@@ -32,28 +33,6 @@ interface IERC735 {
      * Specification: MUST be triggered when changeClaim was successfully called.
      */
     event ClaimChanged(bytes32 indexed claimId, uint256 indexed topic, uint256 scheme, address indexed issuer, bytes signature, bytes data, string uri);
-
-    /**
-     * @dev Definition of the structure of a Claim.
-     *
-     * Specification: Claims are information an issuer has about the identity holder.
-     * The structure should be as follows:
-     *   - claim: A claim published for the Identity.
-     *      - topic: A uint256 number which represents the topic of the claim. (e.g. 1 biometric, 2 residence (ToBeDefined: number schemes, sub topics based on number ranges??))
-     *      - scheme : The scheme with which this claim SHOULD be verified or how it should be processed. Its a uint256 for different schemes. E.g. could 3 mean contract verification, where the data will be call data, and the issuer a contract address to call (ToBeDefined). Those can also mean different key types e.g. 1 = ECDSA, 2 = RSA, etc. (ToBeDefined)
-     *      - issuer: The issuers identity contract address, or the address used to sign the above signature. If an identity contract, it should hold the key with which the above message was signed, if the key is not present anymore, the claim SHOULD be treated as invalid. The issuer can also be a contract address itself, at which the claim can be verified using the call data.
-     *      - signature: Signature which is the proof that the claim issuer issued a claim of topic for this identity. it MUST be a signed message of the following structure: `keccak256(abi.encode(identityHolder_address, topic, data))`
-     *      - data: The hash of the claim data, sitting in another location, a bit-mask, call data, or actual data based on the claim scheme.
-     *      - uri: The location of the claim, this can be HTTP links, swarm hashes, IPFS hashes, and such.
-     */
-    struct Claim {
-        uint256 topic;
-        uint256 scheme;
-        address issuer;
-        bytes signature;
-        bytes data;
-        string uri;
-    }
 
     /**
      * @dev Get a claim by its ID.
