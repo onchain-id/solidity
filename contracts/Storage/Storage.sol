bd93e7824498f1dd520578ac3c839c05be961eac
Adam <boudjemaa.adam@gmail.com>
2020-11-17 21:17:49 +0100

    :add:(Storage, Structs) Separating Variables from Logic

--- contracts/Storage/Storage.sol
+++ contracts/Storage/Storage.sol
@@ -3,13 +3,11 @@ pragma solidity ^0.6.9;
 import "./Structs.sol";
 
 contract Storage is Structs {
-
-
+    string constant private IDENTITY_VERSION = "1.0.0";
     uint256 public constant MANAGEMENT_KEY = 1;
     uint256 public constant ACTION_KEY = 2;
     uint256 public constant CLAIM_SIGNER_KEY = 3;
     uint256 public constant ENCRYPTION_KEY = 4;
-    bool internal identitySettled = false;
     uint256 internal executionNonce;
     mapping(bytes32 => Key) internal keys;
     mapping(uint256 => bytes32[]) internal keysByPurpose;
