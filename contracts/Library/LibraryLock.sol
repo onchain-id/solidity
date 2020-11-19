ef79d72c9a54dbf4a2931fdc01466f50cb9640c9
Adam <boudjemaa.adam@gmail.com>
2020-11-19 11:32:31 +0100

    :fix:(lin) lint

--- contracts/Library/LibraryLock.sol
+++ contracts/Library/LibraryLock.sol
@@ -1,14 +1,14 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.6.9;
 
-import './LibraryLockDataLayout.sol';
+import "./LibraryLockDataLayout.sol";
 
 contract LibraryLock is LibraryLockDataLayout {
     // Ensures no one can manipulate the Logic Contract once it is deployed.
     // PARITY WALLET HACK PREVENTION
 
     modifier delegatedOnly() {
-        require(initialized == true, "The library is locked. No direct 'call' is allowed");
+        require(initialized == true, "The library is locked. No direct call is allowed");
         _;
     }
     function initialize() internal {
--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.6.9;
 
 contract Migrations {
   address public owner;
-  uint public last_completed_migration;
+  uint public lastCompletedMigration;
 
   modifier restricted() {
     if (msg.sender == owner) _;
@@ -14,11 +14,11 @@ contract Migrations {
   }
 
   function setCompleted(uint completed) public restricted {
-    last_completed_migration = completed;
+    lastCompletedMigration = completed;
   }
 
   function upgrade(address new_address) public restricted {
     Migrations upgraded = Migrations(new_address);
-    upgraded.setCompleted(last_completed_migration);
+    upgraded.setCompleted(lastCompletedMigration);
   }
 }
