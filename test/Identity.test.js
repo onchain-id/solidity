bb6325747f0d14804558bb47cbd1ed52bf843197
Adam <boudjemaa.adam@gmail.com>
2020-11-19 11:27:00 +0100

    :fix:(Test) fix tests and edit Identity.test.js to run tests without proxies

--- test/Identity.test.js
+++ test/Identity.test.js
@@ -4,7 +4,6 @@ const { shouldBehaveLikeERC734 } = require('./ERC734.behavior');
 const { shouldBehaveLikeERC735 } = require('./ERC735.behavior');
 
 const Identity = artifacts.require('Identity');
-const IdentityFactory = artifacts.require('IdentityFactory');
 
 contract('Identity', function ([
   identityIssuer,
@@ -12,20 +11,11 @@ contract('Identity', function ([
   claimIssuer,
   anotherAccount,
 ]) {
-  let identityCreated0;
 
   describe('Identity', function () {
     beforeEach(async function () {
       this.identity = await Identity.new({ from: identityIssuer });
-      this.identityFactory = await IdentityFactory.new(this.identity.address, {
-        from: identityIssuer,
-      });
-      identityCreated0 = await this.identityFactory.createIdentity(
-        identityIssuer
-      );
-      this.identity = await Identity.at(
-        identityCreated0.logs[0].args.newIdentityAddress
-      );
+      await this.identity.postConstructor(identityIssuer);
     });
 
     shouldBehaveLikeERC734({
@@ -43,28 +33,5 @@ contract('Identity', function ([
       anotherAccount,
     });
 
-    it('Should be a Cloned Identity', async function () {
-      expect(
-        await this.identityFactory.isClonedIdentity(this.identity.address)
-      ).to.equals(true);
-    });
-
-    it('Should set a new LibraryAddress', async function () {
-      const newIdentity = await Identity.new({ from: identityIssuer });
-      await this.identityFactory.setLibraryAddress(newIdentity.address);
-      expect(await this.identityFactory.libraryAddress()).to.equals(
-        newIdentity.address
-      );
-    });
-
-    it('Should not be able to set an Identity twice', async function () {
-      const identity0 = identityCreated0.logs[0].args.newIdentityAddress;
-      const loadedIdentityWithAnotherAccount = await Identity.at(identity0);
-      await expect(
-        loadedIdentityWithAnotherAccount.set(anotherAccount, {
-          from: anotherAccount,
-        })
-      ).to.be.rejectedWith(Error);
-    });
   });
 });
