245248f16d9129e7d89c4414367da5d031c34cd8
Kevin Thizy <kevin.thizy@intech.lu>
2020-11-27 14:57:30 +0100

    :feat:() Remove duplicated event

--- contracts/Identity.sol
+++ contracts/Identity.sol
@@ -40,8 +40,6 @@ contract Identity is Storage, IIdentity, Version {
         return cs == 0;
     }
 
-    event ExecutionFailed(uint256 indexed executionId, address indexed to, uint256 indexed value, bytes data);
-
     // solhint-disable-next-line func-name-mixedcase
     function __Identity_init(address initialManagementKey) internal {
         require(!initialized || _isConstructor(), "Initial key was already setup.");
