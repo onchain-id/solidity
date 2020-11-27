4eb6342c1124616eef762d320f5d242963ede020
Adam <boudjemaa.adam@gmail.com>
2020-11-27 14:33:28 +0100

    :add:(IIDentity) move events into Interfaces

--- contracts/interface/IIdentity.sol
+++ contracts/interface/IIdentity.sol
@@ -4,4 +4,7 @@ pragma solidity ^0.6.9;
 import "./IERC734.sol";
 import "./IERC735.sol";
 
-interface IIdentity is IERC734, IERC735 {}
+interface IIdentity is IERC734, IERC735 {
+    event ExecutionFailed(uint256 indexed executionId, address indexed to, uint256 indexed value, bytes data);
+    event UpdatedCode(address newContractAddress);
+}
