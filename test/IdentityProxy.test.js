248f35055ea6c295b15dd66ffd879918184bdbd8
Kevin Thizy <kevin.thizy@intech.lu>
2020-11-27 12:14:42 +0100

    :tests:() Add test for preventing interaction with implementation

--- test/IdentityProxy.test.js
+++ test/IdentityProxy.test.js
@@ -1,5 +1,4 @@
 const { expect } = require('chai');
-const abi = require('ethereumjs-abi');
 
 const { shouldBehaveLikeERC734 } = require('./ERC734.behavior');
 const { shouldBehaveLikeERC735 } = require('./ERC735.behavior');
@@ -19,9 +18,9 @@ contract('Identity', function ([
   describe('Identity', function () {
 
     beforeEach(async function () {
-      const identityImplementation = await Identity.new(identityIssuer, true, { from: identityIssuer });
+      this.identityImplementation = await Identity.new(identityIssuer, true, { from: identityIssuer });
       this.implementation = await Implementation.new(
-        identityImplementation.address
+        this.identityImplementation.address
       );
       this.proxy = await Proxy.new(
         this.implementation.address,
@@ -47,19 +46,28 @@ contract('Identity', function ([
       anotherAccount,
     });
 
+    it('Should prevent interaction with the implementation', async function () {
+      expect(this.identityImplementation.removeClaim('0x5fe52eb367804d226afc6386050a629ba0ca6b30bed2f1487dc7afde7db13771')).to.eventually.be.rejectedWith('Returned error: VM Exception while processing transaction: revert Interacting with the library contract is forbidden. -- Reason given: Interacting with the library contract is forbidden..');
+    });
+
     it('Should return version', async function () {
       expect(await this.identity.version()).to.equals("1.0.0");
     });
 
-    it('Should replace the implementation with a new one', async function () {
-      expect((await this.identity.version()).toString()).to.equals('1.0.0');
+    context('when updating the implementation', function() {
+      beforeEach('update implementation', async function() {
+        // Deploy & Replace Implementation on AuthorityImplementation with the new Identity Contract
+        this.newImplementation = await NewIdentity.new(identityIssuer, true, { from: identityIssuer });
+        this.implementation.updateImplementation(this.newImplementation.address);
+      });
 
-      // Deploy & Replace Implementation on AuthorityImplementation with the new Identity Contract
-      this.newImplementation = await NewIdentity.new(identityIssuer, true, { from: identityIssuer });
-      this.implementation.updateImplementation(this.newImplementation.address);
+      it('Should return the updated version', async function () {
+        expect((await this.identity.version()).toString()).to.equals('1.1.0');
+      });
 
-      expect((await this.identity.version()).toString()).to.equals('1.1.0');
+      it('Should still prevent interaction with the implementation', async function () {
+        await expect(this.newImplementation.removeClaim('0x5fe52eb367804d226afc6386050a629ba0ca6b30bed2f1487dc7afde7db13771')).to.eventually.be.rejectedWith('Returned error: VM Exception while processing transaction: revert Interacting with the library contract is forbidden. -- Reason given: Interacting with the library contract is forbidden..');
+      });
     });
-
   });
 });
