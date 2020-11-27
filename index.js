128e347fb4e1b86b1a5d2a534c0f69627dfe5f4c
Kevin Thizy <kevin.thizy@intech.lu>
2020-11-27 12:31:53 +0100

    :feat:() Update exports

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 const IClaimIssuer = require('./build/contracts/IClaimIssuer');
+const IIdentity = require('./build/contracts/IClaimIssuer');
 const IERC734 = require('./build/contracts/IERC734');
 const IERC735 = require('./build/contracts/IERC735');
+
 const ClaimIssuer = require('./build/contracts/ClaimIssuer');
-const ERC734 = require('./build/contracts/ERC734');
 const Identity = require('./build/contracts/Identity');
-const IIdentity = require('./build/contracts/IIdentity');
-const IdentityFactory = require('./build/contracts/IdentityFactory.json');
-const CloneFactory = require('./build/contracts/CloneFactory.json');
+const Proxy = require('./build/contracts/Proxy');
 
 module.exports = {
   contracts: {
     ClaimIssuer,
-    ERC734,
     Identity,
-    IdentityFactory,
-    CloneFactory,
+    Proxy,
   },
   interfaces: {
     IClaimIssuer,
