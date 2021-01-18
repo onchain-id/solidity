8f1b9a0e7be09c0bfcbf08de1ae4b9fc60d85eb1
aboudjem <boudjemaa.adam@gmail.com>
2021-01-18 14:04:56 +0100

    :add:(version) update to 0.8.0

--- contracts/proxy/ImplementationAuthority.sol
+++ contracts/proxy/ImplementationAuthority.sol
@@ -1,11 +1,20 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity ^0.6.9;
+pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "../interface/IImplementationAuthority.sol";
 
-contract ImplementationAuthority is IImplementationAuthority, Ownable {
+contract ImplementationAuthority is IImplementationAuthority {
+
+    address public owner;
+
+    modifier onlyOwner() {
+        require(
+            msg.sender == owner,
+            "This function is restricted to the contract's owner"
+        );
+        _;
+    }
 
     event UpdatedImplementation(address newAddress);
 
@@ -14,6 +23,7 @@ contract ImplementationAuthority is IImplementationAuthority, Ownable {
     constructor(address _implementation) public {
         implementation = _implementation;
         emit UpdatedImplementation(_implementation);
+        owner = msg.sender;
     }
 
     /**
