fe972ff2ef18ec0be627be0aa7aaa53f33b22223
aboudjem <boudjemaa.adam@gmail.com>
2021-01-18 14:06:17 +0100

    :minus:(test) update version + remove unused truffle-proxy + networks

--- truffle-config.js
+++ truffle-config.js
@@ -1,25 +1,17 @@
-const solcStable = {
-  version: '0.6.9',
-};
-
 module.exports = {
   networks: {
-    development: {
-      host: 'localhost',
-      port: 8545,
-      network_id: '*', // eslint-disable-line camelcase
-    },
-    coverage: {
-      host: 'localhost',
-      network_id: '*', // eslint-disable-line camelcase
-      port: 8555,
-      gas: 0xfffffffffff,
-      gasPrice: 0x01,
-    },
   },
 
   compilers: {
-    solc: solcStable,
+    solc: {
+      version: "^0.8.0",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 10000,
+        },
+      },
+    },
   },
-  plugins: ['solidity-coverage', 'truffle-proxy'],
+  plugins: ['solidity-coverage'],
 };
