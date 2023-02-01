a882c27fb8b31094ebb26d5bed0d3de064e8b70d
kevin.thizy <kevin.thizy@intech.lu>
2023-02-01 15:51:45 +0100

    🐛(executions) Fix executions and approval

--- contracts/Identity.sol
+++ contracts/Identity.sol
@@ -115,13 +115,12 @@ contract Identity is Storage, IIdentity, Version {
 
         if (_approve == true) {
             _executions[_id].approved = true;
+            _executions[_id].executed = true;
 
             // solhint-disable-next-line avoid-low-level-calls
             (success,) = _executions[_id].to.call{value:(_executions[_id].value)}(_executions[_id].data);
 
             if (success) {
-                _executions[_id].executed = true;
-
                 emit Executed(
                     _id,
                     _executions[_id].to,
