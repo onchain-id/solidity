85a01bec4329c4830e222bd9ec865feb98960f0d
kevin.thizy <kevin.thizy@intech.lu>
2023-02-06 15:11:15 +0100

    🐛(hardhat) Switch to hardhat tooling

---
+++ hardhat.config.ts
@@ -0,0 +1,9 @@
+import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from "hardhat/config";
+import 'solidity-coverage'
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.17",
+};
+
+export default config;
