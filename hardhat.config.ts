59d6181f6d9c7bcc38c1ce08785b48fea7e81540
kevin.thizy <kevin.thizy@intech.lu>
2023-02-07 11:02:38 +0100

    ➕(lint) Add solhint hardhat plugn

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,7 @@
 import "@nomicfoundation/hardhat-toolbox";
 import { HardhatUserConfig } from "hardhat/config";
-import 'solidity-coverage'
+import 'solidity-coverage';
+import "@nomiclabs/hardhat-solhint";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
