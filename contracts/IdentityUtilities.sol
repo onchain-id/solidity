0f4aa1e37e0abb1de73e1302891d9c3c7329a74c
Adam Boudjemaa <boudjemaa.adam@gmail.com>
2025-10-14 15:11:33 +0400

    Update IdentityUtilities contracts with pragma fixes

--- contracts/IdentityUtilities.sol
+++ contracts/IdentityUtilities.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity 0.8.27;
+pragma solidity ^0.8.27;
 
 import { IIdentityUtilities } from "./interface/IIdentityUtilities.sol";
 import { AccessControlUpgradeable } from "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
--- contracts/_testContracts/TestIdentityUtilities.sol
+++ contracts/_testContracts/TestIdentityUtilities.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity 0.8.27;
+pragma solidity ^0.8.27;
 
 import { IdentityUtilities } from "../IdentityUtilities.sol";
 import { IIdentity } from "../interface/IIdentity.sol";
